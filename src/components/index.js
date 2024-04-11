934c7a08d7a2b72d060f108eb9a2377112a13bd1
mddvisu <davisu8791@gmail.com>
2024-04-11 16:25:41 -0600

    Removed unused file, updated app icon

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,8 @@
-import Layout from './Layout';
 import CloseableTab from './CloseableTab';
 import ClassNode from './ClassNode';
 import SidebarTab from './SidebarTab';
 
 export {
-    Layout,
     CloseableTab,
     SidebarTab,
     ClassNode,
